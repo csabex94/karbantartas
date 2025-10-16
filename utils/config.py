5f703b9a5adb680279c6895b9b83f47059ca90ba
csabex94@ <csabex94@yahoo.com>
2025-10-16 10:10:18 +0300

    ...

--- utils/config.py
+++ utils/config.py
@@ -4,14 +4,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv("../.env"))
+load_dotenv(find_dotenv(".env"))
 
 class Settings(BaseSettings):
     db_url: str
     api_key_secret: str
     jwt_rsa_private_key: str
 
-    model_config = SettingsConfigDict(env_file=".env",extra='ignore', case_sensitive=True)
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
